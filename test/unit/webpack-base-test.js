cbb42e8bc03f42db63497f3c3a9e601cb3d96cbc
liyongjie <liyongjie@tvflnet.com>
2019-08-15 18:43:49 +0800

    【 1. 报错解决了，是项目没安装Istanbul，然后换成windows环境命令看看会不会报错，2.修改单元测试脚本  】

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,7 +6,7 @@ describe('webpack.base.js test case', () => {
 
     console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, 'D:/testProjects/webpack/webpack4-practice/builder-webpack/test/smoke/template/src/pages/index/index.js')
-        assert.equal(baseConfig.entry.search, 'D:/testProjects/webpack/webpack4-practice/builder-webpack/test/smoke/template/src/pages/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('smoke/template/src/pages/index/index.js') > -1, true)
+        assert.equal(baseConfig.entry.search.indexOf('smoke/template/src/pages/search/index.js') > -1, true)
     })
 })
